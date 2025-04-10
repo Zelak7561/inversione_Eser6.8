7fbd1a5c48bca6b0f70b5400d399a7bebc15b836
Zelak7561 <bs0057082@gmail.com>
2025-04-10 19:44:07 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+void lettura(char *s1);
+void scambia(char *s1 , char *str_invertita);
+
+int main() {
+    char str1[20] = "";
+    char str_invertita[20] = "";
+
+    lettura(str1);
+    scambia(str1 , str_invertita);
+
+    cout << "str1 invertita:  " << str_invertita << endl;
+
+    return 0;
+}
+
+void lettura(char *s1 ) {
+    cout << "Inserire la str1: ";
+    cin.getline(s1, 20);
+}
+
+void scambia(char *s1 , char *str_invertita) {
+    int len = 0;
+    for (int i = 0; s1[i] != '\0'; i++) len++;
+
+    for (int i = 0; i < len; i++) {
+        str_invertita[i] = s1[len - 1 - i];
+    }
+}
